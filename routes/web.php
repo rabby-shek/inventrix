34404b533cadf99bdf9df7abd450b30f8bff6355
rabby-shek <rabbyshek1998@gmail.com>
2026-07-11 13:33:08 +0600

    Add login route to web.php with view redirection

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('welcome');
+})->name('login');
+
