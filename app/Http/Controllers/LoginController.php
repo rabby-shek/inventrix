1c340cfd4081a04456e60849caa3c2748092d9c9
rabby-shek <rabbyshek1998@gmail.com>
2026-07-16 11:18:28 +0600

    feat(auth): implement LoginRequest for login validation and enhance LoginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function store(LoginRequest $loginRequest)
+    {
+        if (!Auth::attempt(
+            $loginRequest->only('email', 'password')
+        )) {
+            return back()
+                ->withErrors([
+                    'email' => 'Invalid credentials'
+                ])->onlyInput('email');
+        }
+        $loginRequest->session()->regenerate();
+
+        $loginRequest->user()->update([
+            'last_login_at' => now(),
+        ]);
+
+        return redirect()->intended(route('dashboard'));
+    }
 }
