d6f6c2e64756b22bd4caffc8de1660f1d76191d7
rabby-shek <rabbyshek1998@gmail.com>
2026-07-24 22:07:07 +0600

    feat(customer): add fillable properties for customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    protected $fillable = [
+        "name",
+        'email',
+        'phone',
+        'address',
+        'avatar',
+        'total_spent',
+        'total_orders',
+        'status'
+    ];
 }
